17f4e29d3aa34ee4b7703d5f38677287886e193b
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2023-12-03 11:12:55 -0300

    Add depends

--- src/dependencies/kafka.py
+++ src/dependencies/kafka.py
@@ -1,20 +1,22 @@
 import json
 
 from kafka import KafkaProducer
-from kafka import TopicPartition
 from kafka import KafkaConsumer
 
 from src.infra.envs.envs import get_kafka_group_id, get_kafka_host, get_kafka_port, get_kafka_topic
 
 
-def get_kafka_producer_instance():
+from fastapi import Depends
+
+
+def _get_kafka_producer_instance():
     return KafkaProducer(
         bootstrap_servers=f"{get_kafka_host()}:{get_kafka_port()}",
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
     )
 
 
-def get_kafka_consumer_instance():
+def _get_kafka_consumer_instance():
     consumer = KafkaConsumer(
         get_kafka_topic(),
         group_id=get_kafka_group_id(),
@@ -23,5 +25,16 @@ def get_kafka_consumer_instance():
     for message in consumer:
         print(
             "%s:%d:%d: key=%s value=%s"
-            % (message.topic, message.partition, message.offset, message.key, message.value.decode('utf-8'))
+            % (
+                message.topic,
+                message.partition,
+                message.offset,
+                message.key,
+                message.value.decode("utf-8"),
+            )
         )
+
+
+get_kafka_producer_dependency = Depends(_get_kafka_producer_instance)
+
+get_kafka_consumer_dependency = Depends(_get_kafka_consumer_instance)
