a572aea21d0ebb607d4a9e2df1b14015405194cc
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2023-12-04 09:21:31 -0300

    Add paging

--- src/services/graphql/paging.py
+++ src/services/graphql/paging.py
@@ -2,12 +2,12 @@ from typing import Generic, List, TypeVar
 
 import strawberry
 
-Item = TypeVar("Item")
+Model = TypeVar("Model")
 
 
 @strawberry.type
-class PaginationWindow(Generic[Item]):
-    items: List[Item] = strawberry.field(description="The list of items in this pagination window.")
+class PaginationWindow(Generic[Model]):
+    items: List[Model] = strawberry.field(description="The list of items in this pagination window.")
 
     total_items_count: int = strawberry.field(
         description="Total number of items in the filtered dataset."
