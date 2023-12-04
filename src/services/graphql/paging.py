4983ec8e4c17d2974a96788a7ca8d3040e4f67a5
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2023-12-04 10:35:36 -0300

    Change to T

--- src/services/graphql/paging.py
+++ src/services/graphql/paging.py
@@ -2,12 +2,12 @@ from typing import Generic, List, TypeVar
 
 import strawberry
 
-Agnostic = TypeVar("Agnostic")
+T = TypeVar("T")
 
 
 @strawberry.type
-class PaginationWindow(Generic[Agnostic]):
-    items: List[Agnostic] = strawberry.field(description="The list of items in this pagination window.")
+class PaginationWindow(Generic[T]):
+    items: List[T] = strawberry.field(description="The list of items in this pagination window.")
 
     total_items_count: int = strawberry.field(
         description="Total number of items in the filtered dataset."
