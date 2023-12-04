512193b41f19bfa5dc3cc2896d870ec4f05ec605
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2023-12-04 09:22:21 -0300

    Change to agnostic

--- src/services/graphql/paging.py
+++ src/services/graphql/paging.py
@@ -2,12 +2,12 @@ from typing import Generic, List, TypeVar
 
 import strawberry
 
-Model = TypeVar("Model")
+Agnostic = TypeVar("Agnostic")
 
 
 @strawberry.type
-class PaginationWindow(Generic[Model]):
-    items: List[Model] = strawberry.field(description="The list of items in this pagination window.")
+class PaginationWindow(Generic[Agnostic]):
+    items: List[Agnostic] = strawberry.field(description="The list of items in this pagination window.")
 
     total_items_count: int = strawberry.field(
         description="Total number of items in the filtered dataset."
