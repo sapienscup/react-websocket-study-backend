6c378614d7e70d865d001764ba4131c4f48b9044
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2023-12-18 08:15:24 -0300

    Rm

--- src/infra/chat_companion/assistant.py
+++ src/infra/chat_companion/assistant.py
@@ -1,25 +1,6 @@
-import g4f
-
 from src.contracts.base import BaseContract
-from src.infra.envs.envs import get_gpt_prefix
-
-g4f.debug.logging = True
-g4f.check_version = False
 
 
 class ChatAssistant(BaseContract):
     def perform(self, msg: str):
-        rsp = g4f.ChatCompletion.create(
-            model=g4f.models.gpt_4,
-            messages=[
-                {
-                    "role": "user",
-                    "content": f"{get_gpt_prefix()} {msg}",
-                }
-            ],
-        )
-
-        return self._parse(rsp)
-
-    def _parse(self, rsp: str):
-        return rsp.split('\n')[-1]
+        return "Chat completions inactivated"
