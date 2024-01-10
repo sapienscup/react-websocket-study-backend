bf56917b6c5ebe70e36e1e70a470e2f6697e7822
Lucas Pagotto Tonussi <lptonussi@gmail.com>
2024-01-10 11:48:43 -0300

    Rm

--- src/infra/middleware/cass.py
+++ src/infra/middleware/cass.py
@@ -1,9 +1,4 @@
 import logging
-import time
-
-from cassandra.cqlengine.connection import register_connection, set_default_connection
-
-from src.infra.cass.conn import CassConn
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -12,25 +7,4 @@ logger = logging.getLogger()
 async def fake_middleware(request, call_next):
     logger.info(request)
     logger.info(call_next)
-
-    with CassConn().perform() as context:
-        register_connection(str(context), session=context)
-        set_default_connection(str(context))
-        return await call_next(request)
-
-
-def cassandra_middleware(crud_action):
-    def wrapper():
-        start_time = time.time()
-
-        with CassConn().perform() as context:
-            register_connection(str(context), session=context)
-            set_default_connection(str(context))
-            crud_action()
-
-        end_time = time.time()
-
-        msg = f"[{crud_action.__name__}]\ntotal time: {str(end_time - start_time)}\n"
-        logger.info(msg)
-
-    return wrapper
+    return await call_next(request)
